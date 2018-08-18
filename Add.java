80d0be9559d308614a43d5201ccc7cda40ebda34
Akhilesh Moonat <akhilesh.moonat@gmail.com>
2018-08-18 12:49:27 +0530

    Addition Committed

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter first number:");
+int a=s.nextInt();
+System.out.println("Enter second number:");
+int b=s.nextInt();
+int c=a+b;
+System.out.println("Addition of both numbers:");
+System.out.println(c);
+}
+}
